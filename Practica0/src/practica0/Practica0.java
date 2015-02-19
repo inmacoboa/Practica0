0e497838264387a2ab3838a1d0eab0d508d8e486
inmacoboa <inmacoboa@ei142087>
2015-02-19 13:08:32 +0100

    Commit inicial

--- Practica0/src/practica0/Practica0.java
+++ Practica0/src/practica0/Practica0.java
@@ -16,6 +16,7 @@ public class Practica0 {
      */
     public static void main(String[] args) {
         System.out.println("Hola GIT");
+        System.out.println("Segunda linea en master");
     }
     
 }
